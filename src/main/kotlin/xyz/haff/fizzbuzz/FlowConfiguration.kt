0511b59a78dfa80f53e50cd79ac11266d8ebaeaf
Haf <haf@protonmail.ch>
2023-07-22 12:50:37 +0200

    publishin'

--- src/main/kotlin/xyz/haff/fizzbuzz/FlowConfiguration.kt
+++ src/main/kotlin/xyz/haff/fizzbuzz/FlowConfiguration.kt
@@ -2,20 +2,26 @@ package xyz.haff.fizzbuzz
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.integration.channel.PublishSubscribeChannel
 import org.springframework.integration.dsl.StandardIntegrationFlow
 import org.springframework.integration.dsl.integrationFlow
+import org.springframework.messaging.MessageChannel
 
 @Configuration
 class FlowConfiguration(
     private val redisCounterService: RedisCounterService,
 ) {
 
-
     @Bean
-    fun flow(): StandardIntegrationFlow = integrationFlow({ redisCounterService.next() }, { poller { it.fixedRate(1000).maxMessagesPerPoll(1) }}) {
-        handle {
-            println(it.payload)
-        }
+    fun counterBroadcast(): MessageChannel = PublishSubscribeChannel()
+
+
+    // TODO: Send the polled integers to RabbitMQ
+    @Bean
+    fun flow(counterBroadcast: MessageChannel): StandardIntegrationFlow
+        = integrationFlow({ redisCounterService.next() }, { poller { it.fixedRate(1000).maxMessagesPerPoll(1) }}) {
+        log()
+        channel(counterBroadcast)
     }
 
 }
