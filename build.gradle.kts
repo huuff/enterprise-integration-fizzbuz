3d3bbdb0bba5b33a4811644e2b24d1fab5d8bbf1
Haf <haf@protonmail.ch>
2023-07-30 12:07:35 +0200

    added the test logger plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.2"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
+    id("com.adarshr.test-logger") version "3.2.0"
 }
 
 group = "xyz.haff"
